1b917e052276f5768b2d48e176f5128713c6e89a
Jeff <41554069+JeffreyYST@users.noreply.github.com>
2018-08-26 13:19:08 +1000

    A draft for class Loan

---
+++ loanClass.py
@@ -0,0 +1,48 @@
+class Loan():
+    def __init__(self,name,age,email,has_insurance,start_loan,end_loan,pickup_loc,dropoff_loc):
+ 
+
+        self._customer=Customer(name,age,email) 
+        self._has_insurance=bool
+        self._start_loan=start_loan
+        self._end_loan=end_loan
+        self._pickup_loc=pickup_loc
+        self._dropoff_loc=dropoff_loc
+    
+    def getcustomername(self):
+        return self._customer._name
+
+    def getcustomerage(self):
+        return self._customer._age
+
+    def getcustomeremail(self):
+        return self._customer._email
+
+    def getendloan(self):
+        return self._end_loan
+
+    def getstartloan(self):
+        return self._start_loan
+
+    def getpickuploc(self):
+        return self._pickup_loc
+
+    def getdropoffloc(self):
+        return self._dropoff_loc
+
+    def getprice(self,start_loan,end_loan):
+        period=self._end_loan-self._start_loan
+        self._price= getRentalPrice() *period
+        if getCarType()==large or getCarType()==premium and period >=7:
+            self._price *=0.95
+        if getCarType()==premium:
+            self._price *=1.15
+        return self._price
+
+
+class Customer():
+    def __init__(self,name,age,email):
+        self._name=name
+        self._age=age
+        self._email=email
+        
